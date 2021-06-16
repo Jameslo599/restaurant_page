57c2a8ceb91e12ab2b5b96a00605e6daae1bb11e
Jameslo599 <jameslo599@gmail.com>
2021-06-16 03:43:21 -0500

    able to utilize functions through webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('hello');
+let contents = document.getElementById("content");
+let heading = document.createElement("h1");
+heading.textContent = "Zen Sushi";
+contents.appendChild(heading);
+
