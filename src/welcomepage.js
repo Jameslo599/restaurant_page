33773fc460b03453700b1b251d52b143b9011714
Jameslo599 <jameslo599@gmail.com>
2021-11-03 00:17:36 -0500

    updated info

--- src/welcomepage.js
+++ src/welcomepage.js
@@ -13,19 +13,9 @@ const makeBio = () => {
   biography.item.innerHTML = `We specialize in delicious Japanese cuisine 
     from fresh sushi with a variety of special fish, 
     to hibachi entrees grilled to perfection in the kitchen. <br/><br/>
-    Chef-owner Jack with over 25 years of experience and who previously owned 
-    "Kiraku Japanese Restaurant" in Houston, Texas. 
-    He works hard every day to provide a customized experience for every customer 
-    and treats them like his own family. 
-    When he is not working, Jack enjoys watching movies, singing, 
-    and spending time with his family. <br/><br/>
-    Head sushi-chef Kitamura-san who was previously 
-    the head chef of Jinbei Japanese Restaurant 
-    and has over 40 years of experience working in Japan, Hawaii and New York. 
-    Kitamura-san focuses heavily on ensuring the quality 
-    and standards of our sushi are of the highest order. 
-    Kitamura-san enjoys playing with his cats, 
-    cooking and spending time with his grandchildren.`;
+    Chef-Owner Chien (Jack) with over 27 years of experience and who previously owned "Kiraku Japanese Restaurant" in Houston, Texas. He tries his best to provide a customized experience for every customer and treats them like his own family. <br/><br/>
+    Head Sushi Chef Kitamura was previously the head chef of Jinbei Japanese Restaurant and has over 42 years of experience working in Japan, Hawaii and New York. Kitamura-san focuses heavily on ensuring the quality and standards of our sushi are of the highest order. For this reason, he can be seen as more shy during service.<br/><br/>
+	Head Kitchen Chef Candido with 30 years experience working in teppanyaki restaurants such as Benihana, uses his knowledge to provide food grilled to perfection.`;
   contents.appendChild(biography.item);
 };
 
